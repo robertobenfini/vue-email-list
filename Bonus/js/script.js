a1969bc73cd82f61753ac7bc8445288fab15cf41
robertobenfini <roberto.benfini@gmail.com>
2023-06-19 15:38:44 +0200

    correzione-esercizio

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -2,20 +2,19 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-        return{
-            randomMail: '',
+        return {
             arrayMail: []
         }
     },
-    mounted(){
+    mounted() {
         this.generateMailRandom();
     },
     methods: {
-        generateMailRandom(){
-            for(let i=0; i<10; i++){
+        generateMailRandom() {
+            for (let i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                    this.randomMail = result.data.response
-                    this.arrayMail.push(this.randomMail)
+                    const randomMail = result.data.response
+                    this.arrayMail.push(randomMail)
                 })
             }
         }
